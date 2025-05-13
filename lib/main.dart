8095e11bc78d95fe361215c8ddb87290ef19e633
EmperorXR <deademperor3@gmail.com>
2025-05-13 17:10:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan_8/page/autocomplete.dart';
+import 'package:pertemuan_8/page/profile_page.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Widget> _page = [ProfilePage(), AutocompletePage()];
+
+  var currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: _page[currentPage],
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: currentPage,
+          onTap: (i) => setState(() => currentPage = i),
+          items: [
+            //Profile
+            SalomonBottomBarItem(
+              icon: Icon(Icons.person),
+              title: Text("Profile"),
+              selectedColor: Colors.blue,
+            ),
+
+            //Autocomplete
+            SalomonBottomBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Search'),
+              selectedColor: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
